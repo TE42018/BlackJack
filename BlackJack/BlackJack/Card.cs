513969eb58ba16932967e5fbe98f479ffe4cd65d
Alexandra Persson <viptjejerna@hotmail.com>
2018-10-03 09:47:40 +0200

    Start of BJ
    Klasser

---
+++ BlackJack/BlackJack/Card.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    class Card
+    {
+        public int Value;
+        public SuitType Suit;
+        public int BlackJackValue;
+    }
+}
---
+++ BlackJack/BlackJack/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    class Player
+    {
+        
+
+        public int LowValue { get; set; }
+        public int HighValue { get; set; }
+        public int BestValut { get; set; }
+        
+    }
+
+}
